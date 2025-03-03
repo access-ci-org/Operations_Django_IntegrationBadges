3737e74abc5a9869b3211156c33edc438315f140
Dinuka De Silva <l.dinukadesilva@gmail.com>
2025-03-03 00:13:16 -0500

    Disabling the React.StrictMode for development convenience

--- IntegrationBadgesUI/integration-badges-ui-react/src/index.js
+++ IntegrationBadgesUI/integration-badges-ui-react/src/index.js
@@ -9,9 +9,9 @@ import bootstrap from "bootstrap/dist/js/bootstrap.bundle.min.js";
 const root = ReactDOM.createRoot(document.getElementById('Operations_WebApp_IntegrationBadges'));
 
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
