6da1a519dec5be71c5bac4aadaeed8dbc0c741ec
Dinuka De Silva <l.dinukadesilva@gmail.com>
2025-03-11 11:40:46 -0400

    Update the scripts

--- IntegrationBadgesUI/integration-badges-ui-react/vite.config.js
+++ IntegrationBadgesUI/integration-badges-ui-react/vite.config.js
@@ -2,10 +2,11 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import {dirname, resolve} from 'node:path'
 
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/static/integration-badges-ui-react",
+    // base: "/static/integration-badges-ui-react",
     // publicDir: "/static/integration-badges-ui-react",
     build: {
         manifest: true,
