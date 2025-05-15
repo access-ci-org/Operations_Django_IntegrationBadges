dd0ae5723223cc2ef17315fa5699ce13d50525db
Eric Blau <blau@anl.gov>
2025-05-15 16:09:52 -0500

    make index route login_required

--- IntegrationBadgesUI/urls.py
+++ IntegrationBadgesUI/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="Home"),
+    path("", login_required(views.index), name="Home"),
     path("<path:rest_of_path>", views.index, name="Home"),
 ]
