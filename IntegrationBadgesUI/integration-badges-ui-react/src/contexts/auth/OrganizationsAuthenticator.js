4facd933cb1bd181f491ee4678ff83d1cf2366fe
Dinuka De Silva <L.dinukadesilva@gmail.com>
2025-09-30 16:12:30 -0400

    CTT-395: Changing the org endpoint

--- IntegrationBadgesUI/integration-badges-ui-react/src/contexts/auth/OrganizationsAuthenticator.js
+++ IntegrationBadgesUI/integration-badges-ui-react/src/contexts/auth/OrganizationsAuthenticator.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const organizationsAxiosInstance = axios.create({
-    baseURL: window.SETTINGS.OPERATIONS_API_BASE_URL + window.SETTINGS.OPERATIONS_API_ORGANIZATIONS_PATH
+    baseURL: window.SETTINGS.OPERATIONS_API_BASE_URL + window.SETTINGS.OPERATIONS_API_INTEGRATION_BADGES_PATH
+    // baseURL: window.SETTINGS.OPERATIONS_API_BASE_URL + window.SETTINGS.OPERATIONS_API_ORGANIZATIONS_PATH
 });
