8a87b06d4620ab96c95c4a536276335744a89a6c
Dinuka De Silva <L.dinukadesilva@gmail.com>
2025-10-01 16:17:38 -0400

    CTT-395: Draft concierge dashboard content

--- IntegrationBadgesUI/integration-badges-ui-react/src/pages/concierge/ConciergeDashboard.jsx
+++ IntegrationBadgesUI/integration-badges-ui-react/src/pages/concierge/ConciergeDashboard.jsx
@@ -13,8 +13,12 @@ export default function ConciergeDashboard() {
         fetchResourceRoadmapBadgeStatusSummary,
         getResourceRoadmapBadgeStatusSummary
     } = useResources();
+    const {getRoadmaps} = useRoadmaps();
+    const {getBadges} = useBadges();
 
     const resourceRoadmapBadgeStatusSummary = getResourceRoadmapBadgeStatusSummary();
+    const roadmaps = getRoadmaps();
+    const badges = getBadges();
 
     useEffect(() => {
         fetchResourceRoadmapBadgeStatusSummary();
@@ -29,20 +33,49 @@ export default function ConciergeDashboard() {
         BadgeWorkflowStatus.DEPRECATED
     ]
 
-    console.log("##### resourceRoadmapBadgeStatusSummary: ", resourceRoadmapBadgeStatusSummary)
-
     if (resourceRoadmapBadgeStatusSummary) {
         return <div className="container">
             <div className="row">
                 <h1>Concierge Dashboard</h1>
-                <ul>
-                    {visibleStatusList.map((status) => (<li key={status} className="d-flex flex-row p-2 border-bottom" style={{width: "300px"}}>
-                        <div className="flex-fill"><Translate>badgeWorkflowStatus.{status}</Translate></div>
-                        <Link to={`/concierge/badge-status/?badgeWorkflowStatus=${status}`} className="btn btn-sm btn-dark rounded-2">
-                            {resourceRoadmapBadgeStatusSummary[status] ? resourceRoadmapBadgeStatusSummary[status] : 0}
-                        </Link>
-                    </li>))}
-                </ul>
+            </div>
+            <div className="row">
+                <div className="w-100">
+                    <h2>Roadmaps</h2>
+                </div>
+                {roadmaps && roadmaps.map((roadmap, roadmapIndex) => <div className="col p-2" key={roadmapIndex}>
+                    <div className="bg-light">
+                        {roadmap.name}
+                    </div>
+                </div>)}
+            </div>
+
+            <div className="row">
+                <div className="col-md-8 col-sm-6">
+                    <div className="w-100">
+                        <h2>Badges</h2>
+                    </div>
+                    {badges && badges.map((badge, badgeIndex) => <div className="w-100 p-2" key={badgeIndex}>
+                        <div className="bg-light">
+                            {badge.name}
+                        </div>
+                    </div>)}
+                </div>
+
+                <div className="col-md-4 col-sm-6">
+                    <div className="w-100">
+                        <h2>Concierge Dashboard</h2>
+                    </div>
+                    <ul>
+                        {visibleStatusList.map((status) => (
+                            <li key={status} className="d-flex flex-row p-2 border-bottom" style={{width: "300px"}}>
+                                <div className="flex-fill"><Translate>badgeWorkflowStatus.{status}</Translate></div>
+                                <Link to={`/concierge/badge-status/?badgeWorkflowStatus=${status}`}
+                                      className="btn btn-sm btn-dark rounded-2">
+                                    {resourceRoadmapBadgeStatusSummary[status] ? resourceRoadmapBadgeStatusSummary[status] : 0}
+                                </Link>
+                            </li>))}
+                    </ul>
+                </div>
             </div>
         </div>
     } else {
