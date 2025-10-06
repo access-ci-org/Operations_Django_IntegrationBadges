5145377ea6ac49f708234b67e7ea004d0a7e608e
Dinuka De Silva <L.dinukadesilva@gmail.com>
2025-10-06 13:46:43 -0400

    CTT-632: Updating the vite build

--- IntegrationBadgesUI/integration-badges-ui-react/vite.config.js
+++ IntegrationBadgesUI/integration-badges-ui-react/vite.config.js
@@ -1,14 +1,18 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import image from '@rollup/plugin-image';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), image()],
     build: {
+        assetsInlineLimit: 100000000,
         rollupOptions: {
             output: {
                 dir: '../static/integration-badges-ui-react',
                 // Disable code splitting and create a single entry file
+
+
                 manualChunks: false,
                 inlineDynamicImports: true,
                 entryFileNames: `[name].js`, // Customize the output filename
